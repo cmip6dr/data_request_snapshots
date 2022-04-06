679834620877e2b50ed4bdf1600bbf442a8ffc6b
Martin Juckes <mjuckes@rslmj01.badc.rl.ac.uk>
2022-04-06 11:08:16 +0100

    version 01.00.21

---
+++ Release/README.txt
@@ -0,0 +1,13 @@
+
+
+The dreqPy package includes an XML document specifying the CMIP6 data request and a python API.
+
+The XML document is described in dreqML.pdf in the docs directory.
+
+The python API is described in dreqPy.pdf, also in the docs directory.
+
+
+Version 01.00.21.post1
+----------------------
+Fixes an error in the CMORvar modeling_realm attribute, replacing "ocnBgChem" with "ocnBgchem".
+No other changes (apart from version, document date, and comment line in packageConfig.py).
---
+++ Release/dreqPy/packageConfig.py
@@ -0,0 +1,26 @@
+"""packageConfig
+-------------
+
+Basic information about the package, used by setup.py to populate package metadata.
+"""
+
+import os
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__) )
+
+##DOC_DEFAULT_DIR
+DOC_DEFAULT_DIR = os.path.join(PACKAGE_DIR, 'docs')
+
+DOC_DIR = os.environ.get('DRQ_CONFIG_DIR', DOC_DEFAULT_DIR)
+
+__versionComment__ = "Version 01.00.21.post1 : fixing modeling_realm"
+__version__ = "01.00.21.post1"
+__title__ = "dreqPy"
+__description__ = "CMIP6 Data Request Python API"
+__uri__ = "http://proj.badc.rl.ac.uk/svn/exarch/CMIP6dreq/tags/{0}".format(__version__)
+__author__ = "Martin Juckes"
+__email__ = "martin.juckes@stfc.ac.uk"
+__license__ = "BSD"
+__copyright__ = "Copyright (c) 2015 Science & Technology Facilities Council (STFC)"
+
+version = __version__
